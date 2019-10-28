e8f333852b124a4cde232adf1d59b2a51bcbb23d
Philipp Tessenow <philipp@tessenow.org>
2019-10-28 11:57:26 +0100

    implement block detection and assigning blocks to codels

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -11,36 +11,32 @@ pub const DP_RIGHT: u8 = 1;
 pub const CC_LEFT: u8 = 1;
 
 #[derive(Debug)]
-struct Block {
-    // stores (x,y) coordinates of codels furthest into the DP/CC direction
-    dimensions: (
-        // DP     CP
-        ((u32, u32), (u32, u32)), // up:    up, down
-        ((u32, u32), (u32, u32)), // right: up, down
-        ((u32, u32), (u32, u32)), // down:  up, down
-        ((u32, u32), (u32, u32)), // left:  up, down
-    ),
-    codel_count: u128,
+pub struct Block {
+    codel_coordinates: Vec<(usize, usize)>,
+    hue: u8,
+    light: u8,
 }
 
 impl fmt::Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Block<codel_count: {}, dimensions: {:?}>",
-            self.codel_count, self.dimensions
+            "Block<codel_count: {}, hue: {}, light: {}>",
+            self.codel_coordinates.len(),
+            self.hue,
+            self.light
         )
     }
 }
 
 #[derive(Debug)]
-enum Codel<'a> {
+pub enum Codel {
     Color {
         x: usize,
         y: usize,
         hue: u8,
         light: u8,
-        block: &'a Option<Block>,
+        block_index: Option<usize>,
     },
     Black {
         x: usize,
@@ -50,32 +46,30 @@ enum Codel<'a> {
         x: usize,
         y: usize,
     },
-    Edge,
 }
 
-impl fmt::Display for Codel<'_> {
+impl fmt::Display for Codel {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Codel::Black { x, y } => write!(f, "Codel::Black<x: {}, y: {}>", x, y),
-            Codel::White { x, y } => write!(f, "Codel::White<x: {}, y: {}>", x, y),
+            Codel::Black { x, y } => write!(f, "Codel::Black<{}, {}>", x, y),
+            Codel::White { x, y } => write!(f, "Codel::White<{}, {}>", x, y),
             Codel::Color {
                 x,
                 y,
                 hue,
                 light,
-                block,
+                block_index,
             } => write!(
                 f,
-                "Codel::Color<x: {}, y: {}, hue: {}, light: {}, block: {:?}>",
-                x, y, hue, light, block
+                "Codel::Color<{}, {}> hue: {}, light: {}, block_index: {:?}",
+                x, y, hue, light, block_index
             ),
-            Codel::Edge => write!(f, "Codel::Edge"),
         }
     }
 }
 
 #[derive(Debug)]
-pub struct Interpreter<'a> {
+pub struct Interpreter {
     dp: u8,
     cc: u8,
     alive: bool,
@@ -83,18 +77,18 @@ pub struct Interpreter<'a> {
     step_counter: u128,
     max_steps: u128,
     unlimited_steps: bool,
-    canvas: Vec<Vec<Codel<'a>>>,
+    pub canvas: Vec<Vec<Codel>>,
+    pub blocks: Vec<Block>,
+    width: usize,
+    height: usize,
 }
 
-impl<'a> Interpreter<'a> {
-    pub fn from_rgb_rows(
-        rgb_rows: Vec<Vec<(u8, u8, u8)>>,
-        options: &CmdOptions,
-    ) -> Interpreter<'a> {
+impl Interpreter {
+    pub fn from_rgb_rows(rgb_rows: Vec<Vec<(u8, u8, u8)>>, options: &CmdOptions) -> Interpreter {
         let canvas = create_canvas(rgb_rows, options);
-        let canvas = detect_blocks(canvas);
-
-        Interpreter {
+        let width = canvas[0].len();
+        let height = canvas.len();
+        let mut interpreter = Interpreter {
             dp: DP_RIGHT,
             cc: CC_LEFT,
             alive: true,
@@ -103,7 +97,13 @@ impl<'a> Interpreter<'a> {
             max_steps: options.max_steps,
             unlimited_steps: options.unlimited_steps,
             canvas: canvas,
-        }
+            blocks: Vec::new(),
+            width: width,
+            height: height,
+        };
+        interpreter.detect_blocks();
+        interpreter.assign_codels_to_blocks();
+        interpreter
     }
 
     pub fn is_alive(&self) -> bool {
@@ -113,12 +113,175 @@ impl<'a> Interpreter<'a> {
     pub fn advance(&mut self) -> () {
         self.alive = false;
     }
+
+    pub fn assign_codels_to_blocks(&mut self) -> () {
+        for row in self.canvas.iter_mut() {
+            for codel in row.iter_mut() {
+                if let Codel::Color {
+                    block_index,
+                    x,
+                    y,
+                    hue: _,
+                    light: _,
+                } = codel
+                {
+                    *block_index = self
+                        .blocks
+                        .iter_mut()
+                        .position(|b| b.codel_coordinates.contains(&(*x, *y)));
+                }
+            }
+        }
+    }
+
+    pub fn detect_blocks(&mut self) -> () {
+        let mut visited: Vec<Vec<bool>> = vec![vec![false; self.width]; self.height];
+        for row in self.canvas.iter() {
+            for codel in row.iter() {
+                if let Codel::Color {
+                    block_index: _,
+                    x,
+                    y,
+                    hue,
+                    light,
+                } = codel
+                {
+                    if visited[*y][*x] {
+                        continue;
+                    }
+                    let block = Block {
+                        codel_coordinates: Vec::new(),
+                        hue: *hue,
+                        light: *light,
+                    };
+                    self.blocks.push(block);
+                    let new_block_index = self.blocks.len() - 1;
+
+                    let mut visit_list: Vec<(usize, usize)> = [(*x, *y)].to_vec();
+                    while !visit_list.is_empty() {
+                        let coord = visit_list.pop().unwrap();
+                        if visited[coord.1][coord.0] {
+                            continue;
+                        }
+                        visited[coord.1][coord.0] = true;
+                        let block = &mut self.blocks[new_block_index];
+                        block.codel_coordinates.push(coord);
+
+                        // right neighbour
+                        if let Some(other_coord) = coord_right(coord, self.width, self.height) {
+                            let other_codel = &self.canvas[other_coord.1][other_coord.0];
+                            if let Codel::Color {
+                                block_index: _,
+                                x,
+                                y,
+                                hue,
+                                light,
+                            } = other_codel
+                            {
+                                if !visited[*y][*x] && block.hue == *hue && block.light == *light {
+                                    visit_list.push(other_coord);
+                                }
+                            }
+                        }
+
+                        // left neighbour
+                        if let Some(other_coord) = coord_left(coord, self.width, self.height) {
+                            let other_codel = &self.canvas[other_coord.1][other_coord.0];
+                            if let Codel::Color {
+                                block_index: _,
+                                x,
+                                y,
+                                hue,
+                                light,
+                            } = other_codel
+                            {
+                                if !visited[*y][*x] && block.hue == *hue && block.light == *light {
+                                    visit_list.push(other_coord);
+                                }
+                            }
+                        }
+
+                        // up neighbour
+                        if let Some(other_coord) = coord_up(coord, self.width, self.height) {
+                            let other_codel = &self.canvas[other_coord.1][other_coord.0];
+                            if let Codel::Color {
+                                block_index: _,
+                                x,
+                                y,
+                                hue,
+                                light,
+                            } = other_codel
+                            {
+                                if !visited[*y][*x] && block.hue == *hue && block.light == *light {
+                                    visit_list.push(other_coord);
+                                }
+                            }
+                        }
+
+                        // down neighbour
+                        if let Some(other_coord) = coord_down(coord, self.width, self.height) {
+                            let other_codel = &self.canvas[other_coord.1][other_coord.0];
+                            if let Codel::Color {
+                                block_index: _,
+                                x,
+                                y,
+                                hue,
+                                light,
+                            } = other_codel
+                            {
+                                if !visited[*y][*x] && block.hue == *hue && block.light == *light {
+                                    visit_list.push(other_coord);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
-fn create_canvas<'a>(
-    rgb_rows: Vec<Vec<(u8, u8, u8)>>,
-    options: &CmdOptions,
-) -> Vec<Vec<Codel<'a>>> {
+fn coord_right(coord: (usize, usize), width: usize, _height: usize) -> Option<(usize, usize)> {
+    if coord.0 + 1 >= width {
+        None
+    } else {
+        Some {
+            0: (coord.0 + 1, coord.1),
+        }
+    }
+}
+
+fn coord_left(coord: (usize, usize), _width: usize, _height: usize) -> Option<(usize, usize)> {
+    if coord.0 == 0 {
+        None
+    } else {
+        Some {
+            0: (coord.0 - 1, coord.1),
+        }
+    }
+}
+
+fn coord_up(coord: (usize, usize), _width: usize, _height: usize) -> Option<(usize, usize)> {
+    if coord.1 == 0 {
+        None
+    } else {
+        Some {
+            0: (coord.0, coord.1 - 1),
+        }
+    }
+}
+
+fn coord_down(coord: (usize, usize), _width: usize, height: usize) -> Option<(usize, usize)> {
+    if coord.1 + 1 >= height {
+        None
+    } else {
+        Some {
+            0: (coord.0, coord.1 + 1),
+        }
+    }
+}
+
+fn create_canvas<'a>(rgb_rows: Vec<Vec<(u8, u8, u8)>>, options: &CmdOptions) -> Vec<Vec<Codel>> {
     let mut canvas = Vec::with_capacity(rgb_rows.len());
     for (y, rgb_row) in rgb_rows.into_iter().enumerate() {
         let mut codels = Vec::with_capacity(rgb_row.len());
@@ -130,17 +293,18 @@ fn create_canvas<'a>(
     canvas
 }
 
-fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) -> Codel<'a> {
-    match rgb {
-        (0x00, 0x00, 0x00) => Codel::White { x: x, y: y },
-        (0xFF, 0xFF, 0xFF) => Codel::Black { x: x, y: y },
+fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) -> Codel {
+    eprint!("Inspecting pixel {:?}", rgb);
+    let foo = match rgb {
+        (0x00, 0x00, 0x00) => Codel::Black { x: x, y: y },
+        (0xFF, 0xFF, 0xFF) => Codel::White { x: x, y: y },
         // light red
         (0xFF, 0xC0, 0xC0) => Codel::Color {
             x: x,
             y: y,
             hue: 0,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // red
         (0xFF, 0x00, 0x00) => Codel::Color {
@@ -148,7 +312,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 0,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  red
         (0xC0, 0x00, 0x00) => Codel::Color {
@@ -156,7 +320,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 0,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         // light yellow
         (0xFF, 0xFF, 0xC0) => Codel::Color {
@@ -164,7 +328,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 1,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // yellow
         (0xFF, 0xFF, 0x00) => Codel::Color {
@@ -172,7 +336,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 1,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  yellow
         (0xC0, 0xC0, 0x00) => Codel::Color {
@@ -180,7 +344,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 1,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         // light green
         (0xC0, 0xFF, 0xC0) => Codel::Color {
@@ -188,7 +352,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 2,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // green
         (0x00, 0xFF, 0x00) => Codel::Color {
@@ -196,7 +360,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 2,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  green
         (0x00, 0xC0, 0x00) => Codel::Color {
@@ -204,7 +368,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 2,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         // light cyan
         (0xC0, 0xFF, 0xFF) => Codel::Color {
@@ -212,7 +376,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 3,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // cyan
         (0x00, 0xFF, 0xFF) => Codel::Color {
@@ -220,7 +384,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 3,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  cyan
         (0x00, 0xC0, 0xC0) => Codel::Color {
@@ -228,7 +392,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 3,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         // light blue
         (0xC0, 0xC0, 0xFF) => Codel::Color {
@@ -236,7 +400,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 4,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // blue
         (0x00, 0x00, 0xFF) => Codel::Color {
@@ -244,7 +408,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 4,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  blue
         (0x00, 0x00, 0xC0) => Codel::Color {
@@ -252,7 +416,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 4,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         // light magenta
         (0xFF, 0xC0, 0xFF) => Codel::Color {
@@ -260,7 +424,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 5,
             light: 0,
-            block: &None,
+            block_index: None,
         },
         // magenta
         (0xFF, 0x00, 0xFF) => Codel::Color {
@@ -268,7 +432,7 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 5,
             light: 1,
-            block: &None,
+            block_index: None,
         },
         // dark  magenta
         (0xC0, 0x00, 0xC0) => Codel::Color {
@@ -276,24 +440,27 @@ fn rgb_to_codel<'a>(rgb: (u8, u8, u8), x: usize, y: usize, unknown_white: bool) 
             y: y,
             hue: 5,
             light: 2,
-            block: &None,
+            block_index: None,
         },
         _ => {
+            eprint!(" unknown color");
             if unknown_white {
                 Codel::White { x: x, y: y }
             } else {
                 Codel::Black { x: x, y: y }
             }
         }
-    }
+    };
+    eprintln!(" matched to {}", foo);
+    foo
 }
 
-fn detect_blocks<'a>(canvas: Vec<Vec<Codel<'a>>>) -> Vec<Vec<Codel<'a>>> {
-    canvas
-}
-
-impl fmt::Display for Interpreter<'_> {
+impl fmt::Display for Interpreter {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Interpreter<dp: {}, cc: {}, alive: {}, steps: {}, max_steps: {}, unlimited_steps: {}, stack: {:?}>", self.dp, self.cc, self.alive, self.step_counter, self.max_steps, self.unlimited_steps, self.stack)
+        write!(
+            f,
+            "Interpreter<dp: {}, cc: {}, alive: {}, steps: {}, stack: {:?}>",
+            self.dp, self.cc, self.alive, self.step_counter, self.stack
+        )
     }
 }
